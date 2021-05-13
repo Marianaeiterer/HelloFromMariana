84d519d0afb2da9744a94fe43f1354f24a21055e
Mari Lopes <marieiterer@gmail.com>
2021-05-13 20:20:51 -0300

    Initial commit

---
+++ src/souza/Main.java
@@ -0,0 +1,9 @@
+package souza;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myName = "Mariana";
+        System.out.println("Hello, my name is " + myName);
+    }
+}
