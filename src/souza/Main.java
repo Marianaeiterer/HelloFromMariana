3c0b13ebd504c116a27d6c771a36b6d4e086ac46
Alexis Ortiz <83848928+ortizal19009@users.noreply.github.com>
2021-05-20 10:43:15 -0500

    It's great day I pull my name

--- src/souza/Main.java
+++ src/souza/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         String myName = "Mariana";
         String lucasName = "Lucas";
+        String alName = "Alexis";
         System.out.println("Hello, my name is " + myName);
         System.out.println("Hello " + myName + ", my name is " + lucasName);
+        System.out.println("Hello "+myName+" and "+lucasName+", my name is "+alName);
 
         System.out.println("STRETCH CHALLENGE");
 
