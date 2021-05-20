c7b6c07d013030710989851ea2b5b4aa76731f3c
lmonteiro10 <cdinfo011002>
2021-05-19 23:42:04 -0300

    Line of code added as expected

--- src/souza/Main.java
+++ src/souza/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         String myName = "Mariana";
+        String lucasName = "Lucas";
         System.out.println("Hello, my name is " + myName);
+        System.out.println("Hello " + myName + ", my name is " + lucasName);
     }
 }
